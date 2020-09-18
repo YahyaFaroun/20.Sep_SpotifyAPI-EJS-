e530264db3e8c9b3f7673b3d15445b028f5a7759
Yahya Faroun <yahyafaroun@gmail.com>
2020-09-18 22:42:28 +0200

    finished

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 const SpotifyWebApi = require('spotify-web-api-node');
 require('dotenv').config()
+console.log(process.env)
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static('public'))
 // API key von .env importieren
-// const config = require('./.env')
-// const PORT = process.env.PORT || 5004
-
-
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 3011
+app.listen(PORT, () => {
     console.log('Listening at 3011');
 })
 
@@ -18,7 +16,9 @@ app.listen(process.env.PORT, () => {
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientSecret: process.env.CLIENT_SECRET
+    // clientId: 185f72152e9d443b8d3aa18d84470868,
+    // clientSecret: c9df112077f442c991ef831c93200f27,
 });
 
 // Retrieve an access token
